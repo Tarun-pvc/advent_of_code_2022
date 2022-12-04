53315dfc3ed54babc9f293e25234eef168d8bf13
Tarun <tarun.cosmic@gmail.com>
2022-12-04 11:04:33 +0530

    day 4

---
+++ puzzle4.py
@@ -0,0 +1,44 @@
+import re
+
+
+def partA():
+    total = 0
+    with open("inputFiles/q4Input.txt") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            pairs = re.split(',|-', line)
+            for i in range(len(pairs)):
+                pairs[i] = int(pairs[i])
+            if pairs[3] <= pairs[1] and pairs[2] >= pairs[0]:
+                total += 1
+            elif pairs[3] >= pairs[1] and pairs[2] <= pairs[0]:
+                total += 1
+
+    print(total)
+
+
+def partB():
+    total = 0
+    with open("inputFiles/q4Input.txt") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            pairs = re.split(',|-', line)
+            for i in range(len(pairs)):
+                pairs[i] = int(pairs[i])
+            if pairs[3] <= pairs[1] and pairs[2] >= pairs[0]:
+                total += 1
+            elif pairs[3] >= pairs[1] and pairs[2] <= pairs[0]:
+                total += 1
+            elif pairs[1] >= pairs[2] and pairs[0] <= pairs[2]:
+                total += 1
+            elif pairs[3] >= pairs[0] and pairs[3] <= pairs[1]:
+                total += 1
+
+    print(total)
+
+
+partB()
