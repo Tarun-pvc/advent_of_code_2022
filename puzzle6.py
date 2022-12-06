deef6e77a01da6a9877e158af162df286b071660
Tarun <tarun.cosmic@gmail.com>
2022-12-06 12:45:43 +0530

    day 6

--- puzzle6.py
+++ puzzle6.py
@@ -15,13 +15,17 @@ def repeatCheck(inString):
 
 
 # part A
-for i in range(4, inputLength):
-    if repeatCheck(inputString[i-4:i]) == 0:
-        print(i)
-        break
+def partA():
+    for i in range(4, inputLength):
+        if repeatCheck(inputString[i-4:i]) == 0:
+            return i
 
-# part B
-for i in range(14, inputLength):
-    if repeatCheck(inputString[i-14:i]) == 0:
-        print(i)
-        break
+
+def partB():
+    for i in range(14, inputLength):
+        if repeatCheck(inputString[i-14:i]) == 0:
+            return i
+
+
+print("Day 6 => The answer to part A is: " + partA())
+print("Day 6 => The answer to part B is: " + partB())
